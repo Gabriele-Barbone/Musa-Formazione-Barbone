c305d4faa5b2cf0f2e6644b5fb57b2b37ce25156
Gabriele-Barbone <119305248+Gabriele-Barbone@users.noreply.github.com>
2023-01-24 09:52:26 +0100

    nuovo back

---
+++ arraytest.php
@@ -0,0 +1,15 @@
+<?php
+
+$cars = array ("ferrari","audi","bmw");
+$cars[4] = "toyota";
+$cars[] ="mercedes";
+var_dump($cars)
+
+echo '<hr>';
+
+$ages = array ("mario" => 32, "elena" => 27, "guido" => 29);
+$ages["Pietro"] = 81;
+$ages[] = "Paolo";
+$ages["Maria"] = "16";
+$ages[]
+var_dump($ages)
