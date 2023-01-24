4e82d7eaf334760bcade791943023aace9a18fa6
Gabriele-Barbone <119305248+Gabriele-Barbone@users.noreply.github.com>
2023-01-24 10:55:37 +0100

    aaa

--- arraytest.php
+++ arraytest.php
@@ -3,7 +3,7 @@
 $cars = array ("ferrari","audi","bmw");
 $cars[4] = "toyota";
 $cars[] ="mercedes";
-var_dump($cars)
+var_dump($cars);
 
 echo '<hr>';
 
@@ -11,5 +11,25 @@ $ages = array ("mario" => 32, "elena" => 27, "guido" => 29);
 $ages["Pietro"] = 81;
 $ages[] = "Paolo";
 $ages["Maria"] = "16";
-$ages[]
-var_dump($ages)
+$ages[] = 9;
+$ages[2] = 20;
+var_dump($ages);
+
+echo '<hr>';
+$famiglia = array (
+    "rossi" => array ("1","2","3"),
+    "Verdi" => array ("A","B","C"),);
+
+    $famiglia[] = "pippo";
+
+$famiglia["gialli"] = array ("A","B","C");
+$famiglia[] = 14;
+$famiglia["verdi"][] = "d";
+$famiglia [4][] = "elementoA";
+$famiglia [4][] = "elementoB";
+$famiglia[] = 15;
+$famiglia[3] ="";
+    var_dump ($famiglia);
+
+    $famiglia ["verdi"] = "d";
+
