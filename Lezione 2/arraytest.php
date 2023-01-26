39768d9c94c6d1be23f44799304d42bcd9b75f89
Gabriele-Barbone <119305248+Gabriele-Barbone@users.noreply.github.com>
2023-01-26 11:25:42 +0100

    agg

---
+++ Lezione 2/arraytest.php
@@ -0,0 +1,35 @@
+<?php
+
+$cars = array ("ferrari","audi","bmw");
+$cars[4] = "toyota";
+$cars[] ="mercedes";
+var_dump($cars);
+
+echo '<hr>';
+
+$ages = array ("mario" => 32, "elena" => 27, "guido" => 29);
+$ages["Pietro"] = 81;
+$ages[] = "Paolo";
+$ages["Maria"] = "16";
+$ages[] = 9;
+$ages[2] = 20;
+var_dump($ages);
+
+echo '<hr>';
+$famiglia = array (
+    "rossi" => array ("1","2","3"),
+    "Verdi" => array ("A","B","C"),);
+
+    $famiglia[] = "pippo";
+
+$famiglia["gialli"] = array ("A","B","C");
+$famiglia[] = 14;
+$famiglia["verdi"][] = "d";
+$famiglia [4][] = "elementoA";
+$famiglia [4][] = "elementoB";
+$famiglia[] = 15;
+$famiglia[3] ="";
+    var_dump($famiglia);
+
+    $famiglia ["verdi"] = "d";
+
---
+++ Lezione 2/pasticceriarray.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pasticceria</title>
+</head>
+
+<body>
+    <?php
+    $catalogo = array(
+        "utensili" => array(
+            
+            array(
+                "nome" => "martello",
+                "prezzo" => 7.99
+            ),
+           
+            array(
+                "nome" => "pinza",
+                "prezzo" => 5.99
+            )
+        ),
+        "casalinghi" => array(
+             array(
+                "nome" => "scolapasta",
+                "prezzo" => 1.99
+            )
+        ),
+        "giardinaggio" => array(
+             array(
+                "nome" => "rastrello",
+                "prezzo" => 22.75
+            )
+        ),
+    );
+
+    echo '<hr>';
+    echo "<h4>Elenco utensili</h4>";
+    
+    foreach ($catalogo['utensili'] as $key => $prodotti) {
+           if (is_array($prodotti))
+           {
+            foreach ($prodotti as $key => $prodotto) {
+                echo $prodotto.'<br>';
+
+            }
+           }
+         }
+    echo '<hr>';
+    var_dump($catalogo['casalinghi']);
+    echo '<hr>';
+    var_dump($catalogo['giardinaggio']);
+    echo '<hr>';
+
+    //stampa 5 prodotti contenuti in un array con prezzi in una tabella (visualizza immagine)
+    //bootstrap per la tabella
+    //array è multidimensionale e al primo livello categorie: utensili, casalinghi, giardinaggio -> lista di 5 prodotti nome,descriz, prezzo e url immagine 
+
+    //foreach ($mioArray as $key => $value) {
+    //    echo " chiave {$key} => {$value} ";
+    // }
+    ?>
+
+</body>
+
+</html>
